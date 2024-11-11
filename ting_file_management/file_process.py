a136d99d179758521ee22189de36014ef2dcc711
b166erobot <bernard0freitas@outlook.com>
2024-11-11 07:20:59 -0300

    Requisito 3 satisfeito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
+from .file_management import txt_importer
+
+
+def process(path_file, instance) -> None:
     """Aqui irá sua implementação"""
+    news = txt_importer(path_file)
+    for index in range(len(instance)):
+        old_process = instance.search(index)
+        if old_process['nome_do_arquivo'] == path_file:
+            return
+    new_process = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(news),
+        'linhas_do_arquivo': news,
+    }
+    instance.enqueue(new_process)
+    print(new_process)
 
 
 def remove(instance):
