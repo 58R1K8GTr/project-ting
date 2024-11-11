1b863f3d2ff9f241ee8d362f84eee18005e26dc2
b166erobot <bernard0freitas@outlook.com>
2024-11-11 09:39:49 -0300

    Requisito 7 satisfeito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    results = []
+    for index in range(len(instance)):
+        news = instance.search(index)
+        result = {
+            'palavra': word,
+            'arquivo': news['nome_do_arquivo'],
+            'ocorrencias': []
+        }
+        for number, line in enumerate(news['linhas_do_arquivo'], 1):
+            if word.lower() in line.lower():
+                result['ocorrencias'].append({'linha': number})
+        if len(result['ocorrencias']) > 0:
+            results.append(result)
+    return results
 
 
 def search_by_word(word, instance):
