3cac5c684f61523114dfb09758503444d49455e3
no_name <no@email.com>
2024-11-11 08:51:21 -0300

    Requisito 6 satisfeito

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,18 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from pytest import raises
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    assert len(priority_queue) == 0
+    mocks = list(map(lambda x: {'qtd_linhas': x}, (1, 6, 3)))
+    for count, mock in enumerate(mocks, 1):
+        priority_queue.enqueue(mock)
+        assert len(priority_queue) == count
+    assert priority_queue.search(1) == mocks[2]
+    with raises(IndexError):
+        priority_queue.search(5)
+    assert priority_queue.dequeue() == mocks[0]
+    assert priority_queue.dequeue() == mocks[2]
+    assert priority_queue.dequeue() == mocks[1]
